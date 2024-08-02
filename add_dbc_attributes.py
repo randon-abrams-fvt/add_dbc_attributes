9843d291f75af35b31f3c42042ac08782e5d6a61
Adrian Leon <adrian.leon@epiroc.com>
2024-08-02 07:45:46 -0700

    feat: Add try-except to ensure we can keep going

--- add_dbc_attributes.py
+++ add_dbc_attributes.py
@@ -4,15 +4,18 @@ import argparse
 from attribute_definitions import attribute_def_list
 
 def add_attributes(file):
-    db = cantools.database.load_file(file)
-    print(f"Adding attributes to {file} ...")
-    for message in db.messages:
-        for attribute in attribute_def_list:
-            if attribute.name not in message.dbc.attribute_definitions:
-                message.dbc.attribute_definitions[attribute.name] = attribute
-        break
-    cantools.database.dump_file(db, file)
-    print(f"   ...success!")
+    try:
+        db = cantools.database.load_file(file)
+        print(f"Adding attributes to {file} ...")
+        for message in db.messages:
+            for attribute in attribute_def_list:
+                if attribute.name not in message.dbc.attribute_definitions:
+                    message.dbc.attribute_definitions[attribute.name] = attribute
+            break
+        cantools.database.dump_file(db, file)
+        print(f"   ...success!")
+    except Exception as e:
+        print(f"Error processing {file}: {e}")
 
 def process_files(path):
     if os.path.isdir(path):
