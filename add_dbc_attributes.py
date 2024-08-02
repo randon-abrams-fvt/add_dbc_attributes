59049a32ab113d8eaccd250df22d93c30f2cfbe8
Adrian Leon <adrian.leon@epiroc.com>
2024-08-02 07:13:29 -0700

    feat: Adds the ability to pass an argument

--- add_dbc_attributes.py
+++ add_dbc_attributes.py
@@ -1,5 +1,6 @@
 import os
 import cantools
+import argparse
 from attribute_definitions import attribute_def_list
 
 def add_attributes(file):
@@ -11,13 +12,28 @@ def add_attributes(file):
         break
     cantools.database.dump_file(db, file)
 
+def process_files(path):
+    if os.path.isdir(path):
+        files = filter(os.path.isfile, os.listdir(path))
+        for file in files:
+            if os.path.splitext(file)[1] == ".dbc":
+                if os.access(file, os.W_OK) and os.access(file, os.R_OK):
+                    add_attributes(os.path.join(path, file))
+    elif os.path.isfile(path) and os.path.splitext(path)[1] == ".dbc":
+        if os.access(path, os.W_OK) and os.access(path, os.R_OK):
+            add_attributes(path)
+    else:
+        print(f"Invalid path! {path}")
+
 def main():
-    files = filter(os.path.isfile, os.listdir(os.curdir))
-    for file in files:
-        if os.path.splitext(file)[1] == ".dbc":
-            if os.access(file, os.W_OK) and os.access(file, os.R_OK):
-                add_attributes(file)
-    return
+    parser = argparse.ArgumentParser(description="Add attributes to DBC files.")
+    parser.add_argument("path", nargs="?", default=os.curdir, help="Path to a directory or a DBC file. Default is current directory")
+    args = parser.parse_args()
+
+    if args.path == "?":
+        parser.print_help()
+    else:
+        process_files(args.path)
 
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -2,3 +2,23 @@
 
 The exe in the dist folder will add the attributes used by candoot to every dbc file in the same
 directory as the exe
+
+## Usage
+
+To add attributes to all `.dbc` files in the current directory:
+
+```shell
+python add_dbc_attributes.py
+```
+
+To add attributes to all `.dbc` files in a given path
+
+```shell
+python add_dbc_atributes.py ~/dbc/files/in/this/path
+```
+
+To add attributes to a single `.dbc` file:
+
+```shell
+python add_dbc_attributes.py ~/dbc/file/path.dbc
+```
